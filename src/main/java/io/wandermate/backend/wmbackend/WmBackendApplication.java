9222374aade707dd4c5f1012c1c86b789aee23d0
Shivang Mangal <shivangmangal@gmail.com>
2024-01-24 22:04:55 +0100

    Add todos

--- src/main/java/io/wandermate/backend/wmbackend/WmBackendApplication.java
+++ src/main/java/io/wandermate/backend/wmbackend/WmBackendApplication.java
@@ -11,3 +11,15 @@ public class WmBackendApplication {
 	}
 
 }
+// TODO: 0. Create Jackson ObjectMapper bean
+// TODO: 1. Create model class for UserCreateRequest
+// TODO: 2. Create model class for UserCreateResponse
+// TODO: 3. Create model class for GetUserResponse
+// TODO: 4. Create model class for UpdateUserRequest
+// TODO: Add request validator
+// TODO: Add exception handler
+// TODO: 5. Configure Swagger
+// TODO: 6. Write basic tests
+// TODO: 7. Create docker image of the application
+// TODO - Optional: 8. Add integration tests using TestContainers
+
